acda99c5e1ed12a66775b94aad609cb46a93b89e
LittleLW8847 <31029060+LittleLW8847@users.noreply.github.com>
2024-03-15 02:39:21 +0700

    getting better understanding with export and import comonent

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,36 +1,9 @@
-import ReactImage from './assets/react-core-concepts.png';
+
 import componentsImg from './assets/components.png';
 import {CORE_CONCEPTS} from './data.js';
+import Header from './components/Header.jsx';
+import CoreConcept from './components/CoreConcept.jsx';
 
-const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
-
-function genRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-function Header(){
-  const Description = reactDescriptions[genRandomInt(2)];
-  return (
-    <header>
-        <img src={ReactImage} alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          {Description} React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
-  );
-}
-
-function CoreConcept(props){
-  return (
-    <li>
-      <img src={props.image} alt={props.title}/>
-      <h3>{props.title}</h3>
-      <p>{props.description}</p>
-    </li>
-  );
-}
 
 function App() {
   return (
